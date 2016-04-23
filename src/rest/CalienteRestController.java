ce9f4f9b1fe2c3264d4be22e80aa196397108f08
Alexandre <aalber200@caledonian.ac.uk>
2016-04-23 10:24:12 +0100

    Links do Create / Delete
    just adding quick links to html pages

--- src/rest/CalienteRestController.java
+++ src/rest/CalienteRestController.java
@@ -7,18 +7,15 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import model.Calentada;
 import model.Caliente;
 
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 
 import util.ToModel;
-import dao.CalentadaDAO;
 import dao.CalienteDAO;
 
 @Path("/v1/caliente")
